22845478ecea0dafb3bd653ca12bcbcae19d22f0
Thomas (DJ1TJOO) <t.ferb1@gmail.com>
2026-07-26 18:55:14 +0200

    chore: allow passing to graphql client

--- src/eetlijst/eetlijst.py
+++ src/eetlijst/eetlijst.py
@@ -1,4 +1,6 @@
-from typing import Optional
+from typing import Any, Optional
+
+from httpx import AsyncClient
 
 from eetlijst.generated import GraphQlClient
 
@@ -16,10 +18,30 @@ from eetlijst.services import (
 
 
 class Eetlijst:
-    def __init__(self, api_key: str, url: Optional[str] = None):
+
+    def __init__(
+        self,
+        api_key: str,
+        url: Optional[str] = None,
+        headers: Optional[dict[str, str]] = None,
+        http_client: Optional[AsyncClient] = None,
+        ws_url: str = "",
+        ws_headers: Optional[dict[str, Any]] = None,
+        ws_origin: Optional[str] = None,
+        ws_connection_init_payload: Optional[dict[str, Any]] = None,
+    ):
+        client_headers = dict(headers) if headers else {}
+        if api_key:
+            client_headers["Authorization"] = f"Bearer {api_key}"
+
         self._client = GraphQlClient(
-            url or "https://api.eetlijst.nl/v1/graphql",
-            {"Authorization": f"Bearer {api_key}"},
+            url=url or "https://api.eetlijst.nl/v1/graphql",
+            headers=client_headers,
+            http_client=http_client,
+            ws_url=ws_url,
+            ws_headers=ws_headers,
+            ws_origin=ws_origin,
+            ws_connection_init_payload=ws_connection_init_payload,
         )
 
         self.app = App(self._client)
